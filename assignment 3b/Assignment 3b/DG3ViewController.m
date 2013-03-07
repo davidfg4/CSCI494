01a21344b8429727eb18d3f45143e998932fa8b8
John Bender <john@generalui.com>
2013-03-07 15:08:33 -0800

    grading for assignment 3

--- assignment 3b/Assignment 3b/DG3ViewController.m
+++ assignment 3b/Assignment 3b/DG3ViewController.m
@@ -6,6 +6,8 @@
 //  Copyright (c) 2013 David Gianforte. All rights reserved.
 //
 
+// Good job, 100%. You were supposed to have two navigation controllers in your tab controller, though.
+
 #import "DG3ViewController.h"
 
 @interface DG3ViewController ()
